b16ae71e04de8c16172c457f46dd758d28b20503
Speedy Gonzales <vahid.hedayati@gmail.com>
2013-08-23 17:15:08 +0100

    Create AjaxdependancyselectionGrailsPlugin.groovy

---
+++ AjaxdependancyselectionGrailsPlugin.groovy
@@ -0,0 +1,13 @@
+class AjaxdependancyselectionGrailsPlugin {
+    def version = "0.1"
+    def grailsVersion = "2.0 > *"
+ 
+    def title = "Ajaxdependancyselection Plugin" // Headline display name of the plugin
+    def author = "Vahid Hedayati, Alidad Soleimani"
+	def authorEmail = "badvad@gmail.com"
+	def description = 'Plugin defines secondary auto completion form field values ensuring it is bound on first auto completed form field, initial version written by Alidad'
+	def documentation = "http://grails.org/plugin/ajaxdependancyselection"
+	def license = "GPL2"
+	def issueManagement = [system: 'GITHUB', url: 'https://github.com/vahidhedayati/Ajaxdependancyselection/issues']
+	def scm = [url: 'https://github.com/vahidhedayati/Ajaxdependancyselection']
+}
