674f90a0c2230e5cb0db6fadd0439d6b795d0e3f
Speedy Gonzales <vahid.hedayati@gmail.com>
2013-08-24 15:04:46 +0100

    Added autocomplete and autocompleteSowCollect functions - for single autocomplete tasks

--- grails-app/controllers/ajaxdependancyselection/AutoCompleteController.groovy
+++ grails-app/controllers/ajaxdependancyselection/AutoCompleteController.groovy
@@ -4,6 +4,12 @@ class AutoCompleteController {
 
 	def autoCompleteService
 
+	def autocomplete() {
+		render autoCompleteService.autocompletePrimaryAction(params)
+	 }
+	def autocompleteShowCollect() {
+		render autoCompleteService.autocomplete(params)
+	 }
 	def autocompletePrimaryAction() {
 	   render autoCompleteService.autocompletePrimaryAction(params)
 	}
