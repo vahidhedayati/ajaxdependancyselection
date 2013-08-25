cd57e62101119e390d20831da8b0cf678f868fde
Speedy Gonzales <vahid.hedayati@gmail.com>
2013-08-25 19:35:09 +0100

    Update AjaxdependancyselectionGrailsPlugin.groovy

--- AjaxdependancyselectionGrailsPlugin.groovy
+++ AjaxdependancyselectionGrailsPlugin.groovy
@@ -1,9 +1,9 @@
 class AjaxdependancyselectionGrailsPlugin {
-	def version = "0.4"
+	def version = "0.5"
 	def grailsVersion = "2.0 > *"
  
 	def title = "Ajax Dependancy Selection Plugin"
-	def description = 'Defines secondary auto completion form field values ensuring it is bound on first auto completed form field. Ver 0.3  provides support for select dependancy'
+	def description = 'Defines secondary auto completion form field values ensuring it is bound on first auto completed form field. Ver 0.3  provides support for select dependancy. taglibs to query a grails project controllers and set actions for that controller in select dependancy boxes'
 	def documentation = "http://grails.org/plugin/ajaxdependancyselection"
 	def license = "GPL2"
 	def developers = [
