051e80d4117876985133332e2ffc403a7a3404b1
Vahid H <vahid.hedayati@gmail.com>
2013-09-23 19:33:03 +0100

    Update AjaxdependancyselectionGrailsPlugin.groovy

--- AjaxdependancyselectionGrailsPlugin.groovy
+++ AjaxdependancyselectionGrailsPlugin.groovy
@@ -1,9 +1,9 @@
 class AjaxdependancyselectionGrailsPlugin {
-	def version = "0.19"
+	def version = "0.20"
 	def grailsVersion = "2.0 > *"
  
 	def title = "Ajax Dependancy Selection Plugin"
-	def description = 'Defines next auto completion/selection form field values ensuring it is bound on previous auto completed/selected form field. This can be used on two or more objects of hasMany and belongsTo. Provides: g:autocomplete, g:autoCompletePrimary, g:autoCompleteSecondary, g:selectPrimary, g:selectSecondary &  g:selectController.'
+	def description = 'Defines next auto completion/selection form field values ensuring it is bound on previous auto completed/selected form field. This can be used on two or more objects of hasMany and belongsTo. Provides: g:autocomplete, g:autoCompletePrimary, g:autoCompleteSecondary, g:autoCompleteSecondaryNR, g:selectPrimary, g:selectSecondary , g:selectSecondaryNR &  g:selectController.'
 	def documentation = "http://grails.org/plugin/ajaxdependancyselection"
 	def license = "GPL2"
 	def developers = [
