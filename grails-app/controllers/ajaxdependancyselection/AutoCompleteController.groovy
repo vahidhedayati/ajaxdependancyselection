aa1cdd6222b10b75d64a521c07b93bb9a20eee1e
Vahid <vahid.hedayati@gmail.com>
2014-03-02 03:13:27 +0000

    tidyup

--- grails-app/controllers/ajaxdependancyselection/AutoCompleteController.groovy
+++ grails-app/controllers/ajaxdependancyselection/AutoCompleteController.groovy
@@ -7,43 +7,48 @@ class AutoCompleteController {
 	
 	def ajaxSelectControllerAction() { 
 		render autoCompleteService.returnControllerActions(params)
-	}	
+	}
+		
 	def ajaxSelectSecondary() {
 		render autoCompleteService.selectSecondary(params)
 	}
+	
 	def ajaxSelectSecondaryNR() {
 		render autoCompleteService.selectSecondaryNR(params)
 	}
+	
 	def autocomplete() {
 		render autoCompleteService.autocompletePrimaryAction(params)
 	}
+	
 	def autocompleteShowCollect() {
 		render autoCompleteService.autocomplete(params)
 	}
+	
 	def autocompletePrimaryAction() {
 	   	render autoCompleteService.autocompletePrimaryAction(params)
 	}
+	
 	def autocompleteSecondaryAction() {
 	   	render autoCompleteService.autocompleteSecondaryAction(params)
 	}
+	
 	def autocompleteSecondaryNR() {
 		render autoCompleteService.autocompleteSecondaryNR(params)
 	}
+	
 	def loadFilterWord() {
-		
 		render (template: '/autoComplete/filterWord',  model: [params:params])
 	}
+	
 	def loadFilterWord2() {
-		if (params.template) {
-			render (template: params.template,  model: [params:params])
-		}else{
-			render (template: '/autoComplete/filterWord2',  model: [params:params])
-		}
+		render (template: '/autoComplete/filterWord2',  model: [params:params])
 	}
+	
 	def returnPrimarySearch(){
 		render autoCompleteService.returnPrimarySearch('json',params.term,params.filterType,params.domain,params)
-	//	render autoCompleteService.returnPrimarySearch('json',params.term,params.domain,params)
 	}
+	
 	def secondarySearch() {
 		render autoCompleteService.secondarySearch(params)
 	}
