c0b97d182d92eb54cffa5689dd222364f2150120
vahidhedayati <badvad@gmail.com>
2015-06-29 20:55:20 +0100

    license update!

--- AjaxdependancyselectionGrailsPlugin.groovy
+++ AjaxdependancyselectionGrailsPlugin.groovy
@@ -5,7 +5,7 @@ class AjaxdependancyselectionGrailsPlugin {
 	def title = "Ajax Dependancy Selection Plugin"
 	def description = 'Defines next auto completion/selection form field values ensuring it is bound on previous auto completed/selected form field. This can be used on two or more objects of hasMany and belongsTo. Provides: g:autocomplete, g:autoCompletePrimary, g:autoCompleteSecondary, g:autoCompleteSecondaryNR, g:selectPrimary, g:selectSecondary , g:selectSecondaryNR &  g:selectController. g:selectAutoComplete and g:selectPrimaryNR. Now also supporting 1 object with multiple dependencies.'
 	def documentation = "http://grails.org/plugin/ajaxdependancyselection"
-	def license = "GPL2"
+	def license = "APACHE"
 	def developers = [
 		[name: "Vahid Hedayati", email: "badvad@gmail.com"],
 		[name: "Alidad Soleimani", email: "badvad@gmail.com"]
